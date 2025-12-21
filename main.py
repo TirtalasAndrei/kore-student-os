d017830caecd02b9cbc295431980d483bb4d2197
Tirtalas Andrei <tirtalasandrei24@gmail.com>
2025-12-21 16:10:47 +0200

    Dashboard in progress

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/dashboard')
+def aplication():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
