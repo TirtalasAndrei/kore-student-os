41a52a39cf351fa62d34353c8b1e3c2f456afe75
Tirtalas Andrei <tirtalasandrei24@gmail.com>
2025-12-22 17:33:07 +0200

    Add SQL database

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notite.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=True)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
