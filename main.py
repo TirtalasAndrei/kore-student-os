29d85c9197a62f31f005eac61f8ab0155ef31719
Tirtalas Andrei <tirtalasandrei24@gmail.com>
2025-12-22 17:49:42 +0200

    Try to make the SQL database work and save something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
 
 app = Flask(__name__)
 
@@ -23,6 +24,31 @@ def home():
 @app.route('/dashboard')
 def aplication():
     return render_template("dashboard.html")
+
+@app.route('/save_note', methods=['POST'])
+def save_note():
+    data = request.json
+    content_as_string = json.dumps(data)
+
+    nota = Note.query.get(1)
+
+    if nota:
+        nota.content = content_as_string
+    else:
+        nota = Note(id=1, content=content_as_string)
+        db.session.add(nota)
+
+    db.session.commit()
+    return jsonify({"status": "success", "message": "Salvat in baza de date!"})
+
+@app.route('/get_note', methods=['GET'])
+def get_note():
+    nota = Note.query.get(1)
+
+    if nota and nota.content:
+        return jsonify(json.loads(nota.content))
+    else:
+        return jsonify({})
 
 if __name__ == "__main__":
     app.run(debug=True)
